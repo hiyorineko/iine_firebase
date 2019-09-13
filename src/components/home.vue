daf4f753d584f42101bf43af4d77de02fbd00be3
hiyoriaya <hiyoriaya@gmail.com>
2019-09-13 23:15:40 +0900

    再取得追加

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,9 +2,6 @@
     <div class="home">
         <input v-model="text" type="text">
         <button @click="input">登録</button>
-        <p v-if="showMessage" style="margin-top: 20px">
-          フォームが正常に送信されました。
-        </p>
         <div class="user-list" v-for="(text, index) in texts" :key='index'>
             <p>{{text.text}}</p>
         </div>
@@ -26,20 +23,23 @@ export default {
             firebase.firestore().collection('text').doc().set({
                 text: this.text,
             })
-            this.showMessage = true
+            this.showInput()
+            this.text = ""
+        },
+        showInput(){
+            firebase.firestore().collection('text').get().then((querySnapshot) => {
+                this.loading = false
+                querySnapshot.forEach((doc) => {
+                    let data = {
+                        'text': doc.data().text
+                    }
+                    this.texts.push(data)
+                })
+           })
         }
     },
   created () {
-       firebase.firestore().collection('text').get().then((querySnapshot) => {
-            this.loading = false
-            querySnapshot.forEach((doc) => {
-                let data = {
-                    'text': doc.data().text
-                }
-                this.texts.push(data)
-            })
-       })
-       console.log(this.texts)
+       this.showInput()
    }
 }
 </script>

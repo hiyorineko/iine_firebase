932a367ec106694b26650ab5baec16ddb1742a30
hiyoriaya <hiyoriaya@gmail.com>
2019-09-13 23:08:35 +0900

    登録内容表示

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,6 +2,12 @@
     <div class="home">
         <input v-model="text" type="text">
         <button @click="input">登録</button>
+        <p v-if="showMessage" style="margin-top: 20px">
+          フォームが正常に送信されました。
+        </p>
+        <div class="user-list" v-for="(text, index) in texts" :key='index'>
+            <p>{{text.text}}</p>
+        </div>
     </div>
 </template>
 <script>
@@ -11,6 +17,8 @@ export default {
   data(){
     return{
       text: '',
+      texts: [],
+      showMessage: false
     }
   },
   methods:{
@@ -18,8 +26,21 @@ export default {
             firebase.firestore().collection('text').doc().set({
                 text: this.text,
             })
+            this.showMessage = true
         }
-    }
+    },
+  created () {
+       firebase.firestore().collection('text').get().then((querySnapshot) => {
+            this.loading = false
+            querySnapshot.forEach((doc) => {
+                let data = {
+                    'text': doc.data().text
+                }
+                this.texts.push(data)
+            })
+       })
+       console.log(this.texts)
+   }
 }
 </script>
 
